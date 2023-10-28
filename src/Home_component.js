5c90542f0842c7fb3a24fa5eedb4a7f476a940c9
ADmi <alexander.starkin@gmail.com>
2023-10-28 12:40:26 +0300

    [INIT] ALEX

---
+++ src/Home_component.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import satelite from './img/satelite.jpg'
+import planet from './img/planet.jpg'
+import './App.css';
+import './Home_component.css'
+
+function Home_component() {
+    return (
+      <body className="Home_component">
+        <main className="Home_component_header">
+          <img src={logo} className="App-logo planet" alt="logo" />
+
+
+            <div>
+            <div className="satelite">
+                <img src = {satelite}></img>
+            </div>
+            <div>
+                <img src = {planet}></img>
+            </div>
+            </div>
+            
+        </main>
+      </body>
+    );
+  }
+  export default Home_component;
